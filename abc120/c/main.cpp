44daa0f43bacaebc78cd1a2786bab665e2a597f5
K <kamiyaowl@gmail.com>
2019-03-03 22:18:48 +0900

    やっぱりTLE

--- abc120/c/main.cpp
+++ abc120/c/main.cpp
@@ -11,21 +11,6 @@
 
 using namespace std;
 
-// 置換場所検索用
-std::vector<int> find_all(const std::string str, const std::string subStr) {
-    std::vector<int> result;
- 
-    int subStrSize = subStr.size();
-    int pos = str.find(subStr);
- 
-    while (pos != std::string::npos) {
-        result.push_back(pos);
-        pos = str.find(subStr, pos + subStrSize);
-    }
- 
-    return result;
-}
-
 map<string, int> reduce_memo { {"", 0} , {"1", 0}, {"0", 0}, };
 int reduce(const string str) {
     // メモしてある結果
@@ -33,47 +18,59 @@ int reduce(const string str) {
         // cout << "[dbg] mem_ret result:" << reduce_memo[str] << " " << str << endl;
         return reduce_memo[str];
     }
-    // 取り除けそうな場所を列挙
-    auto targets = find_all(str, "01");
-    auto targets2 = find_all(str, "10");
-    targets.insert(targets.end(), targets2.begin(), targets2.end());
-    // なければ終了
-    if (targets.size() == 0) {
-        return 0;
-    } else {
-        // あれば一番いいところを採用
-        int max_index = -1;
-        int max_result = -1;
-        // cout << "[dbg] targets.size():" << targets.size() << endl;
 
-        set<string> calced;
-        for(const int index: targets) {
-            string tmp(str);
-            tmp.replace(index, 2, "");
-            // すでにやった内容と一致している可能性があるので枝刈りする
-            if(calced.count(tmp) > 0) {
-                continue;
-            }
-            calced.insert(tmp);
-            // いい結果を
-            auto result = reduce(tmp) + 2;
-            // cout << "\t[dbg] result:" << result << " " << str << " -> " << tmp << endl;
-            if (max_result < result) {
-                max_index = index;
-                max_result = result;
-            }
-        }
+    // 順番に検索
+    set<string> calced;
+    int max_index = -1;
+    int max_result = -1;
+    for(int last_index = 0; ;) {
+        int index = str.find("01", last_index);
+        if (index == string::npos) break;
+        last_index = index + 1;
 
-        // 一番良い結果
-        if (max_index < 0) {
-            cout << "Not Implemented" << endl;
-            return -1;
+        string tmp(str);
+        tmp.replace(index, 2, "");
+        // すでにやった内容と一致している可能性があるので枝刈りする
+        if(calced.count(tmp) > 0) {
+            continue;
         }
-        // メモって返す
-        // cout << "[dbg] new_ret result:" << max_result << " " << str << endl;
-        reduce_memo[str] = max_result;
-        return max_result;
+        calced.insert(tmp);
+        // いい結果を
+        auto result = reduce(tmp) + 2;
+        // cout << "\t[dbg] result:" << result << " " << str << " -> " << tmp << endl;
+        if (max_result < result) {
+            max_index = index;
+            max_result = result;
+        }
     }
+    for(int last_index = 0; ;) {
+        int index = str.find("10", last_index);
+        if (index == string::npos) break;
+        last_index = index + 1;
+
+        string tmp(str);
+        tmp.replace(index, 2, "");
+        // すでにやった内容と一致している可能性があるので枝刈りする
+        if(calced.count(tmp) > 0) {
+            continue;
+        }
+        calced.insert(tmp);
+        // いい結果を
+        auto result = reduce(tmp) + 2;
+        // cout << "\t[dbg] result:" << result << " " << str << " -> " << tmp << endl;
+        if (max_result < result) {
+            max_index = index;
+            max_result = result;
+        }
+    }
+    // 置換不可能
+    if (max_index < 0) {
+        return 0;
+    }
+    // メモって返す
+    // cout << "[dbg] new_ret result:" << max_result << " " << str << endl;
+    reduce_memo[str] = max_result;
+    return max_result;
 }
 int main(void) {
     string str;
