12d8199fdd6e4cc75fa1d62cde1a4793f068f82d
K <kamiyaowl@gmail.com>
2019-03-03 22:00:01 +0900

    多少削減したけどまだTLE

--- abc120/c/main.cpp
+++ abc120/c/main.cpp
@@ -30,6 +30,7 @@ map<string, int> reduce_memo { {"", 0} , {"1", 0}, {"0", 0}, };
 int reduce(const string str) {
     // メモしてある結果
     if (reduce_memo.count(str) > 0) {
+        // cout << "[dbg] mem_ret result:" << reduce_memo[str] << " " << str << endl;
         return reduce_memo[str];
     }
     // 取り除けそうな場所を列挙
@@ -43,10 +44,18 @@ int reduce(const string str) {
         // あれば一番いいところを採用
         int max_index = -1;
         int max_result = -1;
+        // cout << "[dbg] targets.size():" << targets.size() << endl;
 
+        set<string> calced;
         for(const int index: targets) {
             string tmp(str);
             tmp.replace(index, 2, "");
+            // すでにやった内容と一致している可能性があるので枝刈りする
+            if(calced.count(tmp) > 0) {
+                continue;
+            }
+            calced.insert(tmp);
+            // いい結果を
             auto result = reduce(tmp) + 2;
             // cout << "\t[dbg] result:" << result << " " << str << " -> " << tmp << endl;
             if (max_result < result) {
@@ -61,6 +70,7 @@ int reduce(const string str) {
             return -1;
         }
         // メモって返す
+        // cout << "[dbg] new_ret result:" << max_result << " " << str << endl;
         reduce_memo[str] = max_result;
         return max_result;
     }
