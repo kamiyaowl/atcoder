81b2cf5bf130e99eec08821d83d999e4e8433320
K <kamiyaowl@gmail.com>
2019-03-03 22:40:23 +0900

    間に合わんかったし多分だめ

--- abc120/c/main.cpp
+++ abc120/c/main.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cmath>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <set>
 #include <tuple>
 #include <stdbool.h>
@@ -11,75 +11,53 @@
 
 using namespace std;
 
-// 置換場所検索用
-std::vector<int> find_all(const std::string str, const std::string subStr) {
-    std::vector<int> result;
- 
-    int subStrSize = subStr.size();
-    int pos = str.find(subStr);
- 
-    while (pos != std::string::npos) {
-        result.push_back(pos);
-        pos = str.find(subStr, pos + subStrSize);
+// 左から順に処理
+tuple<int, string> reduce1(string str) {
+    int count = 0;
+    for(int last_index = 0; ;) {
+        cout << str<< endl;
+        int index1 = str.find("01", last_index);
+        int index2 = str.find("10", last_index);
+        if (index1 != string::npos && index2 == string::npos) {
+            str.replace(index1, 2, "");
+            last_index = index1;
+        } else if (index2 != string::npos && index1 == string::npos) {
+            str.replace(index2, 2, "");
+            last_index = index1;
+        } else if (index1 != string::npos && index1 < index2) {
+            str.replace(index1, 2, "");
+            last_index = index1;
+        } else if (index2 != string::npos && index2 < index1) {
+            str.replace(index1, 2, "");
+            last_index = index1;
+            count++;
+        } else {
+            break;
+        }
     }
- 
-    return result;
+    return {count * 2, str};
 }
+int reduce2(string str) {
+    int sum = 0;
+    int score = 0;
 
-map<string, int> reduce_memo { {"", 0} , {"1", 0}, {"0", 0}, };
-int reduce(const string str) {
-    // メモしてある結果
-    if (reduce_memo.count(str) > 0) {
-        // cout << "[dbg] mem_ret result:" << reduce_memo[str] << " " << str << endl;
-        return reduce_memo[str];
-    }
-    // 取り除けそうな場所を列挙
-    auto targets = find_all(str, "01");
-    auto targets2 = find_all(str, "10");
-    targets.insert(targets.end(), targets2.begin(), targets2.end());
-    // なければ終了
-    if (targets.size() == 0) {
-        return 0;
-    } else {
-        // あれば一番いいところを採用
-        int max_index = -1;
-        int max_result = -1;
-        // cout << "[dbg] targets.size():" << targets.size() << endl;
+    string dst;
+    dst = str;
+    do {
+        // cout << "#####################" << endl;
+        auto t = reduce1(dst);
+        score = get<0>(t);
+        dst = get<1>(t);
+        sum += score;
 
-        set<string> calced;
-        for(const int index: targets) {
-            string tmp(str);
-            tmp.replace(index, 2, "");
-            // すでにやった内容と一致している可能性があるので枝刈りする
-            if(calced.count(tmp) > 0) {
-                continue;
-            }
-            calced.insert(tmp);
-            // いい結果を
-            auto result = reduce(tmp) + 2;
-            // cout << "\t[dbg] result:" << result << " " << str << " -> " << tmp << endl;
-            if (max_result < result) {
-                max_index = index;
-                max_result = result;
-            }
-        }
-
-        // 一番良い結果
-        if (max_index < 0) {
-            cout << "Not Implemented" << endl;
-            return -1;
-        }
-        // メモって返す
-        // cout << "[dbg] new_ret result:" << max_result << " " << str << endl;
-        reduce_memo[str] = max_result;
-        return max_result;
-    }
+    } while(score > 0);
+    return sum;
 }
 int main(void) {
     string str;
     cin >> str;
 
-    auto result = reduce(str);
+    auto result = reduce2(str);
     cout << result << endl;
     return 0;
 }
