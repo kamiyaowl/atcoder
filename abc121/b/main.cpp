63f64d2bcbf7e70aa1cae72bd89a1ef7e2d6175c
K <kamiyaowl@gmail.com>
2019-03-11 17:05:06 +0900

    abc121 b ac

--- abc121/b/main.cpp
+++ abc121/b/main.cpp
@@ -12,5 +12,27 @@
 using namespace std;
 
 int main(void) {
+    int n, m, c;
+    cin >> n;
+    cin >> m;
+    cin >> c;
+    vector<int> b(m);
+    for(int i = 0 ; i < m ; ++i) {
+        cin >> b[i];
+    }
+    int ans = 0;
+    for(int j = 0 ; j < n ; ++j) {
+        int spec = c;
+        for(int i = 0 ; i < m ; ++i) {
+            int a;
+            cin >> a;
+            spec += a * b[i];
+        }
+        if (spec > 0) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+
     return 0;
 }
