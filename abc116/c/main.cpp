997d61c8a9a98273a7783d469d6965d865d896bd
K <kamiyaowl@gmail.com>
2019-03-17 22:08:14 +0900

    abc116 c ac

--- abc116/c/main.cpp
+++ abc116/c/main.cpp
@@ -15,24 +15,29 @@ using namespace std;
 
 int mizuyari(vector<int> h) {
     // 0が含まれない区間で分割統治する
+    // for(const auto& x : h) {
+    //     cout << x << ", ";
+    // }
+    // cout << endl;
+
     int count = 0;
     for(int i = 0 ; i < h.size() ; ) {
         if (!h[i]) {
             i++;
             continue;
         }
+        vector<int> sub; // 分割統治するvector
         int j;
         for(j = i ; j < h.size() ; ++j ) {
             if(!h[j]) {
                 break;
+            } else {
+                sub.push_back(h[j] - 1); // 水やりする
             }
         }
-        // (i, j]区間でいけるので高さを減らす
-        for(int x = i ; x < j ; ++x) {
-            h[x]--;
-        }
+        // (i, j]区間でいける
         count++; // 今回の水やり分
-        count += mizuyari(h); // 水をやった後の残りはまかした
+        count += mizuyari(sub); // 水をやった後の残りはまかした
         // 後処理
         i = j;
     }
