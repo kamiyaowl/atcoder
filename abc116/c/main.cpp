322c83e4bc2928b9b1f59c2ecd8f52de5e78083d
K <kamiyaowl@gmail.com>
2019-03-17 12:12:33 +0900

    abc116 c 途中

--- abc116/c/main.cpp
+++ abc116/c/main.cpp
@@ -13,7 +13,40 @@
 namespace mp = boost::multiprecision;
 using namespace std;
 
+int mizuyari(vector<int> h) {
+    // 0が含まれない区間で分割統治する
+    int count = 0;
+    for(int i = 0 ; i < h.size() ; ) {
+        if (!h[i]) {
+            i++;
+            continue;
+        }
+        int j;
+        for(j = i ; j < h.size() ; ++j ) {
+            if(!h[j]) {
+                break;
+            }
+        }
+        // (i, j]区間でいけるので高さを減らす
+        for(int x = i ; x < j ; ++x) {
+            h[x]--;
+        }
+        count++; // 今回の水やり分
+        count += mizuyari(h); // 水をやった後の残りはまかした
+        // 後処理
+        i = j;
+    }
+    return count;
+}
 
 int main(void) {
+    int n;
+    cin >> n;
+    vector<int> h(n);
+    for(int i = 0 ; i < n ; ++i) {
+        cin >> h[i];
+    }
+    auto ans = mizuyari(h);
+    cout << ans << endl;
     return 0;
 }
