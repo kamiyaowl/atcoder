89986fc841b06a18ff07ac9770f52bcb1a7130d3
K <kamiyaowl@gmail.com>
2019-04-06 22:02:24 +0900

    abc123 c ac

--- abc123/c/main.cpp
+++ abc123/c/main.cpp
@@ -15,20 +15,28 @@ using namespace std;
 
 
 int main(void) {
-    int n;
+    mp::cpp_int n;
     cin >> n;
 
-    vector<int> arr(5);
-    vector<int> term(5);
-    int result = -1;
-    // それぞれなんターン必要？
+    vector<mp::cpp_int> arr(6); // 移動時間
+    vector<mp::cpp_int> term(6); // ターン数
+    arr[0] = INT64_MAX;
+    term[0] = 0; // dummy
     for(int i = 0 ; i < 5 ; ++i) {
-        cin >> arr[i];
-        term[i] = (n / arr[i]) + ((n % arr[i]) == 0 ? 0 : 1) + i; // iは人間が隣町に移動するターンオフセット
-        if (term[i] > result) {
-            result = term[i];
+        cin >> arr[i + 1];
+    }
+
+    for(int i = 0 ; i < 5 ; ++i) {
+        if (arr[i + 1] > arr[i]) {
+            // 前回の移動手段のほうが遅いので、そっちに引っ張られる
+            term[i + 1] = term[i] + 1; // +1は1ターンのディレイ
+        } else {
+            mp::cpp_int t = (n / arr[i + 1]) + (n % arr[i + 1] == 0 ? 0 : 1) + i;
+            mp::cpp_int recent = term[i] + 1;
+            term[i + 1] = t < recent ? recent : t;
         }
     }
+    auto result = term[term.size() - 1];
     cout << result << endl;
     return 0;
 }
