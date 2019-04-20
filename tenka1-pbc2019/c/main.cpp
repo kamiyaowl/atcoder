30521be1684e72e75c7ba21312c5f96ae81bf25a
K <kamiyaowl@gmail.com>
2019-04-20 22:12:49 +0900

    tenka c wa

--- tenka1-pbc2019/c/main.cpp
+++ tenka1-pbc2019/c/main.cpp
@@ -13,12 +13,51 @@
 namespace mp = boost::multiprecision;
 using namespace std;
 
+// 左にあるbの数>wの数であればbwをデクリメントして
 
+int replace(string& s) {
+    int b = 0; // 現時点より左にある#の数
+    int count = 0;
+    for(int i = 0 ; i < s.length() ; ++i) {
+        if (s[i] == '#') {
+            b++;
+            if (s[i + 1] == '.') {
+                // #.なので処置が必要。
+                if(b < 2) {
+                    // ここより左はbがなさそうなので、i-1をb->wにする
+                    s[i - 1] = '.';
+                    count++;
+                    // ..になるのでこれより後ろはbに怯えないためクリア
+                    b = 0;
+                } else {
+                    //###.....みたいなパターン。最寄りのbかwどちらかが消えるまで置き換えが必要？
+                    int j;
+                    for(j = i + 1 ; (j < s.length()) && (s[j] == '.') ; ++j) {}
+                    int w = j - (i + 1); // 右にあるwの数
+                    if (b < w) {
+                        // 手前のbすべてwに変更
+                        count += b;
+                        b = 0;
+                    } else {
+                        // この後の連続するwをbに変更
+                        count += w;
+                        b += w; // 黒が増えるので注意
+                    }
+                    // すすめる
+                    i = j - 1; // increment文を引いておく
+                }
+            }
+        } else {
+            // .なのでbをクリア
+            b = 0;
+        }
+    }
+    return count;
+}
 int main(void) {
     int n;
     string s; // '#' or '.'
     cin >> n >> s;
-    string s2(s);
 
     // 短い場合
     if (n == 1) {
@@ -29,33 +68,9 @@ int main(void) {
         return 0;
     }
 
-    // main
-    int count = 0;
-    for(int i = 0 ; i < n ; ++i) {
-        auto before = (i > 0) ? s[i-1] : '.'; // 無害
-        auto current = s[i];
-        auto next =  (i < n - 1) ? s[i] : '#'; // 境界なので、最後は無害なので黒にしておく
-        // 関係ない
-        if (before != '#') continue;
-        if (current != '.') continue;
-        // 処置がいる
-        s[i] = '#';
-        count++;
-    }
-    int count2 = 0;
-    for(int i = 0 ; i < n ; ++i) {
-        auto before = (i > 0) ? s2[i-1] : '.'; // 無害
-        auto current = s2[i];
-        auto next =  (i < n - 1) ? s2[i] : '#'; // 境界なので、最後は無害なので黒にしておく
-        // 関係ない
-        if (before != '#') continue;
-        if (current != '.') continue;
-        // 処置がいる
-        s2[i-1] = '.';
-        count2++;
-    }
     // result
-    cout << min(count, count2) << endl;
+    auto result = replace(s);
+    cout << result << endl;
 
     return 0;
 }
